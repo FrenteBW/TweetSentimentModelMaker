b9bc3946e012eb2dbcfd22db78c6c70e197480fb
An Byungwook <abw2619@icloud.com>
2022-05-10 09:47:44 +0900

    Initial Commit

---
+++ TweetSentimentModelMaker/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  TweetSentimentModelMaker
+//
+//  Created by 안병욱의 mac on 2022/05/10.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ TweetSentimentModelMaker/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TweetSentimentModelMaker
+//
+//  Created by 안병욱의 mac on 2022/05/10.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
